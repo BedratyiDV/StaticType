f53e2b3de61ea3d04f1bbede90ac755a0aa7a4b5
BedratyiDV <bedrik2023@gmail.com>
2023-10-15 23:31:40 +0200

    First Commit
    15.10.2023 The First Commit

---
+++ StaticType/Point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticType
+{
+    internal class Point
+    {
+        private int _xCoordinate;
+        private int _yCoordinate;
+        internal static int Counter;
+
+        /// <summary>
+        /// Getter-Setter for X coordinate field. It's possible to set only positive values from 0 to Int.MaxValue
+        /// </summary>
+        public int XCoordinate
+        {
+            get => _xCoordinate;
+            set => _xCoordinate = value < int.MaxValue && value > 0 ? value : 0;
+        }
+        /// <summary>
+        /// Getter-Setter for Y coordinate field. It's possible to set only positive values from 0 to Int.MaxValue
+        /// </summary>
+        public int YCoordinate
+        {
+            get => _yCoordinate;
+            set => _yCoordinate = value < int.MaxValue && value > 0 ? value : 0;
+        }
+        /// <summary>
+        /// Constructor for Point class initializing
+        /// </summary>
+        /// <param name="xCoordinate">   </param>
+        /// <param name="yCoordinate"> </param>
+
+        public Point(int xCoordinate, int yCoordinate)
+        {
+            _xCoordinate = xCoordinate < int.MaxValue && xCoordinate > 0 ? xCoordinate : 0;
+            _yCoordinate = yCoordinate < int.MaxValue && yCoordinate > 0 ? yCoordinate : 0;
+            Counter++;
+        }
+    }
+}
---
+++ StaticType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace StaticType
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Point point1 = new Point(-7, 12);
+           Point point2 = new Point(340, -70);
+            point2.XCoordinate = -10;
+            point2.YCoordinate = 20;
+
+            Console.WriteLine($"Coordinats of point1: X = {point1.XCoordinate}, Y = {point1.YCoordinate}");
+            Console.WriteLine($"Coordinats of point1: X = {point2.XCoordinate}, Y = {point2.YCoordinate}");
+            Console.WriteLine($"Counter current value: Counter = {Point.Counter}");
+
+            Point point3 = new Point(17, 8);
+            Point point4 = new Point(2, 31);
+
+            Console.WriteLine($"Counter current value: Counter = {Point.Counter}");
+            Console.WriteLine($"Distance between point 3 and point 4 is" +
+                    $" {Calculation.GetDestancebetweenPoints(point3, point4)}");
+
+            Calculation calculation = new(); 
+            Console.WriteLine($"Distance between internal point and point 3 is" +
+                    $" {calculation.GetDistanceToPoint(point3)}");
+            Console.WriteLine($"Distance between internal point and point 4 is" +
+                    $" {calculation.GetDistanceToPoint(point4)}");
+
+
+
+        }
+    }
+}
